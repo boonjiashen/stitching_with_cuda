26bc286412491e6a3aa5efa068727f7c4c535313
Jia-Shen Boon <jiashen@gmail.com>
2015-12-12 11:00:10 -0600

    Test unified memory - doesn't seem to be supported

--- submitToSlurm.sh
+++ submitToSlurm.sh
@@ -6,29 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=q2
-#SBATCH -o outConvolution_%j
+#SBATCH -o out%j
 
 cd $SLURM_SUBMIT_DIR
-
-#./convolution.out 32
-#./convolution.out 64
-#./convolution.out 128
-#./convolution.out 256
-#./convolution.out 512
-#./convolution.out 1024
-#./convolution.out 2048
-
-#filename="v5.txt"
-#rm -f $filename
-#touch $filename
-echo \<root\>
-for i in `seq 4 12`;
-do
-    N=$((2**i))
-    echo N = 2^$i = $N
-    echo \<run ID=$N\>
-    ./convolution.out $N
-    echo \<\/run\>
-    echo -------------
-done   
-echo \<\/root\>
+./cudaFeatureMatcher
