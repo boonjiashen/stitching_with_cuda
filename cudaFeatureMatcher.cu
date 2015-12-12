372426a60502091de1d76aeb1dbc2b2a06b94823
Jia-Shen Boon <jiashen@gmail.com>
2015-12-12 12:38:35 -0600

    Add source of GPU assert checker

--- cudaFeatureMatcher.cu
+++ cudaFeatureMatcher.cu
@@ -3,6 +3,9 @@
 #include <opencv2/opencv.hpp>
 #include "Matrix.h"
 
+/*
+ *Source: http://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+ */
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool
         abort=true)
