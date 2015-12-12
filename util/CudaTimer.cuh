8bc48c80b564c886470b4281f12b1cbcda46f0ca
Jia-Shen Boon <jiashen@gmail.com>
2015-12-11 20:32:09 -0600

    Add util files from other repo

---
+++ util/CudaTimer.cuh
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+using namespace std;
+
+class CudaTimer {
+
+    private:
+        cudaEvent_t inclusiveStart, inclusiveStop;
+        bool ticked;  // If tic has been run
+
+    public:
+
+    CudaTimer() {
+        cudaEventCreate(&inclusiveStart);
+        cudaEventCreate(&inclusiveStop);
+        ticked = false;
+    }
+    ~CudaTimer() {
+        cudaEventDestroy(inclusiveStart);
+        cudaEventDestroy(inclusiveStop);
+    }
+
+    void tic() {
+        cudaEventRecord(inclusiveStart, 0);
+        ticked = true;
+    }
+
+    float toc() {
+        assert(ticked);
+        float inclusive_timing; 
+        cudaEventRecord(inclusiveStop, 0);
+        cudaEventSynchronize(inclusiveStop);
+        cudaEventElapsedTime(&inclusive_timing, inclusiveStart, inclusiveStop);
+        return inclusive_timing;
+    }
+};
