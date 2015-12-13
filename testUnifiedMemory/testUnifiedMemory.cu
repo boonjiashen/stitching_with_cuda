75139c96d0c2952e516878c1b741b97a33f666db
Jia-Shen Boon <jiashen@gmail.com>
2015-12-12 21:48:24 -0600

    Add function to display CUDA stats programmatically

--- testUnifiedMemory/testUnifiedMemory.cu
+++ testUnifiedMemory/testUnifiedMemory.cu
@@ -13,7 +13,38 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool
     }
 }
 
+void DisplayHeader()
+{
+    const int kb = 1024;
+    const int mb = kb * kb;
+    std::wcout << "NBody.GPU" << std::endl << "=========" << std::endl << std::endl;
+
+    std::wcout << "CUDA version:   v" << CUDART_VERSION << std::endl;    
+
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    std::wcout << "CUDA Devices: " << std::endl << std::endl;
+
+    for(int i = 0; i < devCount; ++i)
+    {
+        cudaDeviceProp props;
+        cudaGetDeviceProperties(&props, i);
+        std::wcout << "Device " << i << ": " << props.name << ": compute capability " << props.major << "." << props.minor << std::endl;
+        std::wcout << "  Global memory:   " << props.totalGlobalMem / mb << "mb" << std::endl;
+        std::wcout << "  Shared memory:   " << props.sharedMemPerBlock / kb << "kb" << std::endl;
+        std::wcout << "  Constant memory: " << props.totalConstMem / kb << "kb" << std::endl;
+        std::wcout << "  Block registers: " << props.regsPerBlock << std::endl << std::endl;
+
+        std::wcout << "  Warp size:         " << props.warpSize << std::endl;
+        std::wcout << "  Threads per block: " << props.maxThreadsPerBlock << std::endl;
+        std::wcout << "  Max block dimensions: [ " << props.maxThreadsDim[0] << ", " << props.maxThreadsDim[1]  << ", " << props.maxThreadsDim[2] << " ]" << std::endl;
+        std::wcout << "  Max grid dimensions:  [ " << props.maxGridSize[0] << ", " << props.maxGridSize[1]  << ", " << props.maxGridSize[2] << " ]" << std::endl;
+        std::wcout << std::endl;
+    }
+}
+
 int main(void) {
+    DisplayHeader();
 
     int n = 10;  // sum of all feature counts
     int k = 4;  // size of one descriptor
