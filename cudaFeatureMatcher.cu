4e83fb3bfc642e21e2a79a37266c29a5739236a0
Jia-Shen Boon <jiashen@gmail.com>
2015-12-14 16:36:58 -0600

    Write GPU version of computing correspondence Mat
    haven't tested it yet though.

--- cudaFeatureMatcher.cu
+++ cudaFeatureMatcher.cu
@@ -239,10 +239,77 @@ void computeCorrespondenceMat(const Matrix<float> distanceMat,
     for (int imgIdx = 0; imgIdx < numImages; ++imgIdx) {
         int start = imgIdx > 0 ? cumNumDescriptors[imgIdx-1] : 0;
         int stop = cumNumDescriptors[imgIdx];
-        Matrix<float> src = getSubmatrix(distanceMat, start, stop);
-        Matrix<int> dst = getSubmatrix(correspondenceMat, imgIdx, imgIdx + 1);
+        Matrix<float> src = getSubmatrix<float>(distanceMat, start, stop);
+        Matrix<int> dst = getSubmatrix<int>(correspondenceMat, imgIdx, imgIdx + 1);
 
         computeCorrespondenceVec(src, dst, matchConfidence);
+    }
+}
+
+/*
+ *See gpuComputeCorrespondenceVec()
+ *Pre-conditions:
+ *    Every thread is responsible for computing one element in
+ *    `correspondenceMat`
+ */
+__global__ void gpuComputeCorrespondenceVec(
+        const Matrix<float> distanceSubmat,
+        Matrix<int> correspondenceMat, float matchConfidence) {
+    int n = distanceSubmat.width;  // no. of descriptors matched from
+    int n_i = distanceSubmat.height;  // no. of descriptors matched to
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    if (tid >= n) { return; }
+
+    float dist1 = FLT_MAX, dist2 = FLT_MAX; // smallest & 2nd smallest distance
+    int idx1 = -1;  // index of match with smallest distance
+
+    for (int j = 0; j < n_i; ++j) {
+        float currDist = distanceSubmat.elements[j * n + tid];
+
+        if (currDist < dist1) {
+            dist2 = dist1;
+            dist1 = currDist;
+            idx1 = j;
+        }
+        else if (currDist < dist2) {
+            dist2 = currDist;
+        }
+    }
+
+    if (dist1 / dist2 < 1 - matchConfidence) {
+        correspondenceMat.elements[tid] = idx1;
+    }
+    else {
+        correspondenceMat.elements[tid] = -1;
+    }
+}
+
+
+/*
+ *See computeCorrespondenceMat()
+ *Pre-conditions:
+ *    `distanceMat` and `correspondenceMat` are in device memory
+ *    `cumNumDescriptors` is in host memory
+ */
+void gpuComputeCorrespondenceMat(const Matrix<float> distanceMat,
+        int* cumNumDescriptors,
+        int numImages, Matrix<int> correspondenceMat, float matchConfidence) {
+    assert(distanceMat.height == distanceMat.width);
+    assert(correspondenceMat.height == numImages);
+    assert(correspondenceMat.width == distanceMat.width);
+    assert(cumNumDescriptors[numImages-1] == distanceMat.height);
+
+    int n = distanceMat.height;
+
+    dim3 dimGrid((n + BLOCK_SIZE - 1)/BLOCK_SIZE, n);
+    dim3 dimBlock(BLOCK_SIZE);
+    for (int imgIdx = 0; imgIdx < numImages; ++imgIdx) {
+        int start = imgIdx > 0 ? cumNumDescriptors[imgIdx-1] : 0;
+        int stop = cumNumDescriptors[imgIdx];
+        Matrix<float> src = getSubmatrix<float>(distanceMat, start, stop);
+        Matrix<int> dst = getSubmatrix<int>(correspondenceMat, imgIdx, imgIdx + 1);
+
+        gpuComputeCorrespondenceVec<<<dimGrid,dimBlock>>>(src, dst, matchConfidence);
     }
 }
 
@@ -327,8 +394,8 @@ void printCorrespondence(const Matrix<float>& allDescriptors,
 
 int main(void) {
 
-    /*const int numImages = 3;*/
-    /*int numDescriptors[] = {2, 3, 4};*/
+    const int numImages = 3;
+    int numDescriptors[] = {2, 3, 4};
     int k = 32;  // size of one descriptor
     int n = 1000;  // sum of all feature counts
     /*float matchConf = 0.1;*/
