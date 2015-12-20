41cdcb798c8ebbe3b7174854b1e484f275f6093b
Jia-Shen Boon <jiashen@gmail.com>
2015-12-19 21:11:47 -0600

    Add cpu timings to plot

--- scripts/plot.py
+++ scripts/plot.py
@@ -4,43 +4,73 @@ import xml_to_sqlite_connection as x2s
 
 INPUT = None
 
+class DataGetter():
+
+    @staticmethod
+    def get(filename, select_statement):
+        with open(filename) as fid:
+            textblock = fid.read()
+        textblock = "<body>\n" + textblock + "\n</body>"
+
+        # Create SQL table from XML file
+        con = x2s.xml_to_sqlite_connection(textblock)
+        cur = con.cursor()
+
+        # Enables stdev functionality
+        # Need to first compile extension-functions.c with
+        # gcc -g -shared -fPIC extension-functions.c -o extension-functions.so
+        con.enable_load_extension(True)
+        con.execute("select load_extension('./extension-functions.so')")
+        con.enable_load_extension(False)
+
+        # Get data amenable to plotting
+        result_tuple = zip(*cur.execute(select_statement).fetchall())
+        return tuple(np.array(x) for x in result_tuple)
+
+    @staticmethod
+    # NOTE: Returns timing in seconds
+    def get_cpu_timing(filename):
+        select_statement = """
+        SELECT numImages*numDescriptorsPerImage as n, timingMS
+        FROM tbl
+        """
+        nn, tt = DataGetter.get(filename, select_statement)
+        return nn, tt/1000
+
+    @staticmethod
+    # NOTE: Returns timing in seconds
+    def get_gpu_timing(filename):
+        select_statement = """
+        SELECT numImages*numDescriptorsPerImage as n, inclusiveTimingMS, exclusiveTimingMS
+        FROM tbl
+        """
+        nn, inclusive, exclusive = DataGetter.get(filename, select_statement)
+        return nn, inclusive/1000, exclusive/1000
+
+
 def main():
 
-    textblock = INPUT.read()
-    textblock = "<body>\n" + textblock + "\n</body>"
-
-    # Create SQL table from XML file
-    con = x2s.xml_to_sqlite_connection(textblock)
-    cur = con.cursor()
-
-    # Enables stdev functionality
-    # Need to first compile extension-functions.c with
-    # gcc -g -shared -fPIC extension-functions.c -o extension-functions.so
-    con.enable_load_extension(True)
-    con.execute("select load_extension('./extension-functions.so')")
-    con.enable_load_extension(False)
-
-    # Get data amenable to plotting
-    select_statement = """
-    SELECT numImages*numDescriptorsPerImage as n, inclusiveTimingMS
-    FROM tbl
-    """
-    nn, tt = zip(*cur.execute(select_statement).fetchall())
-    print(nn, tt)
+    cpu_n, cpu_timing_in_s = DataGetter.get_cpu_timing('../timingFiles/cpu.stdout')
+    gpu_n, gpu_inclusive_in_s, gpu_exclusive_in_s =  \
+            DataGetter.get_gpu_timing('../timingFiles/88878.stdout')
 
     # Plot
     plt.figure()
     ax = plt.gca()
     ax.set_xscale('log', basex=10)
     ax.set_yscale('log', basex=10)
-    ax.plot(nn, tt, 'o-')
-    ax.plot([1, 2], [1, 2])
-    #plt.ylim(0, plt.ylim()[-1])
-    #plt.xlim(0, plt.xlim()[-1])
+    ax.set_aspect('equal')
+    ax.plot(cpu_n, cpu_timing_in_s, 'o')
+    ax.plot(gpu_n, gpu_inclusive_in_s, 'o-')
+    #ax.plot(gpu_n, gpu_exclusive_in_s, 'o')
+    #plt.ylim(1, plt.ylim()[-1])
+    #plt.xlim(1, plt.xlim()[-1])
     plt.show()
     
 if __name__ == "__main__":
-    #input = sys.stdin
-    INPUT = open('../timingFiles/88878.stdout')
     main()
-    INPUT.close()
+    #input = sys.stdin
+    #INPUT = open('../timingFiles/88878.stdout')
+    #INPUT = open('../timingFiles/cpu.stdout')
+    #main()
+    #INPUT.close()
